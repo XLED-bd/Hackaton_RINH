e6faed705cbf086f16a6f6b386ae9d791b940cd5
AbsoluteSolver <dkarter513@gmail.com>
2023-11-23 20:35:54 +0300

    test

--- app/src/main/java/com/example/hackaton_rinh/MainActivity.java
+++ app/src/main/java/com/example/hackaton_rinh/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.hackaton_rinh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView = findViewById(R.id.halloworld);
+        textView.setText("AbobaHallo");
         int x = 5;
         int y = 666;
     }
