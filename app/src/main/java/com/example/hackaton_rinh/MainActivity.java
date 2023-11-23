2a8e8b67922733755f792bd9b2f1b519c9da178e
XLED-bd <korol.gorox@gmail.com>
2023-11-23 19:53:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hackaton_rinh/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hackaton_rinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int x = 2;
+    }
+}
